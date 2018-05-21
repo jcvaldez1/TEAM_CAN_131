05505e219d271e5a03fd81f4af9e0e66114743cd
Jonathan Cancio <jonahcancio@gmail.com>
2018-05-21 14:31:48 +0800

    curve fitting equation now solved

--- JavaStuff/PvmmCurveFitter.java
+++ JavaStuff/PvmmCurveFitter.java
@@ -1,3 +1,8 @@
+/*To run this program:
+	javac PvmmCurveFitter.java
+	java PvmmCurveFitter <file_name.csv>
+*/
+
 import java.util.ArrayList;
 import Jama.*;
 import org.joda.time.*;
@@ -8,51 +13,78 @@ public class PvmmCurveFitter{
 	CsvToMatrixParser ctmp;
 	ArrayList<String[]> parseList;
 
-	Matrix closingPriceVector;
+
 	Matrix previousPriceVector;
 	Matrix previousVolumeVector;
 	Matrix momentum22Vector;
 	Matrix momentum7Vector;
 
-	Matrix finalMatrix;
+	Matrix finalMatrixA;
+	Matrix closingPriceVectorB;
+	Matrix finalVectorX;
 
 	public static void main(String args[]){
 		PvmmCurveFitter pcf = new PvmmCurveFitter();
-		pcf.theRealMain();
+		pcf.theRealMain(args);
 	}
 
-	public void theRealMain(){
+	public void theRealMain(String args[]){
 		ctmp = new CsvToMatrixParser();
-		parseList = ctmp.parseCsvToList("ap_data.csv");
+		//parseList = ctmp.parseCsvToList("ap_data.csv");
+		try{
+			parseList = ctmp.parseCsvToList(args[0]);
+		}catch(ArrayIndexOutOfBoundsException e){
+			System.out.println("No arguments given; Must run program like this: java PvmmCurveFitter <file_name.csv>");
+			System.exit(1);
+		}
 		initVectorsAndMatrices();
-		printFinalMatrix();
+		//printFinalMatrixA();
+		//finalVectorX = finalMatrixA.solve(closingPriceVectorB);
+		//finalVectorX.print(15, 12);//print final answer with minimum 15 digits, and 12 digits after decimal point
+		printFinalAnswerX();
 	}
 
+	//initializes the matrix and vectos needed for Ax = b
 	public void initVectorsAndMatrices(){
-		closingPriceVector = getClosingPrice();
+		closingPriceVectorB = getClosingPrice();
 		previousPriceVector = getPreviousPrice();
 		previousVolumeVector = getPreviousVolume();
 		momentum22Vector = getMomentum(22);
 		momentum7Vector = getMomentum(7);
 
-		Matrix concat = concatLR(closingPriceVector, previousPriceVector);
-		concat = concatLR(concat, previousVolumeVector);
-		concat = concatLR(concat, momentum22Vector);
-		finalMatrix = concatLR(concat, momentum7Vector);
+		finalMatrixA = concatLR(previousPriceVector, previousVolumeVector);
+		finalMatrixA = concatLR(finalMatrixA, momentum22Vector);
+		finalMatrixA = concatLR(finalMatrixA, momentum7Vector);
 	}
 
-	public void printFinalMatrix(){
+	//prints out final answer from vectorX
+	public void printFinalAnswerX(){
+		finalVectorX = finalMatrixA.solve(closingPriceVectorB);
+		double pPrev = finalVectorX.get(0, 0);
+		double vPrev = finalVectorX.get(1, 0);
+		double mom22 = finalVectorX.get(2, 0);
+		double mom7 = finalVectorX.get(3, 0);
+		System.out.printf(
+			"Pnow =\n\t%14.12f  Pprev\n"
+			+ "  +\t%14.12f  Vprev\n"
+			+ "  +\t%14.12f  mom22\n"
+			+"  +\t%14.12f  mom7\n", pPrev, vPrev, mom22, mom7
+		);
+	}
+
+	//prints out Matrix A in tabular format, for debugging
+	public void printFinalMatrixA(){
 		System.out.println("Pnow\tPprev\tVprev\t\tmom22\tmom7");
-		for(int i = 0; i < finalMatrix.getRowDimension(); i++){
-			double pNow = finalMatrix.get(i, 0);
-			double pPrev = finalMatrix.get(i, 1);
-			double vPrev = finalMatrix.get(i, 2);
-			double mom22 = finalMatrix.get(i, 3);
-			double mom7 = finalMatrix.get(i, 4);
-			System.out.printf("%.2f\t%.2f\t%9.0f\t%.4f\t%.4f\n", pNow, pPrev, vPrev, mom22, mom7);
+		for(int i = 0; i < finalMatrixA.getRowDimension(); i++){
+			double pPrev = finalMatrixA.get(i, 0);
+			double vPrev = finalMatrixA.get(i, 1);
+			double mom22 = finalMatrixA.get(i, 2);
+			double mom7 = finalMatrixA.get(i, 3);
+			System.out.printf("%.2f\t%9.0f\t%.4f\t%.4f\n", pPrev, vPrev, mom22, mom7);
 		}
 	}
 
+	//concats matrix a with matrix b on right, like: c = [a, b] in matlab
 	public Matrix concatLR(Matrix a, Matrix b){
 		if(a.getRowDimension() != b.getRowDimension()){
 			System.out.println("matrix rows don't match");
@@ -66,18 +98,19 @@ public class PvmmCurveFitter{
 		return merge;
 	}
 
-
-
+	//returns closing price vector, requires parseList initalized first
 	public Matrix getClosingPrice(){
 		int colIndex[] = {0};
 		return ctmp.parseListToMatrix(parseList, colIndex);
 	}
 
+	//returns previous price vector, requires parseList initalized first
 	public Matrix getPreviousPrice(){
 		int colIndex[] = {11};
 		return ctmp.parseListToMatrix(parseList, colIndex);
 	}
 
+	//returns previous volume vector, requires parseList initalized first
 	public Matrix getPreviousVolume(){
 		int colIndex[] = {5};
 		Matrix origV = ctmp.parseListToMatrix(parseList, colIndex);
@@ -87,6 +120,7 @@ public class PvmmCurveFitter{
 		return prevV;
 	}
 
+	//returns momentum vector at specificed delta, requires parseList and closingPriceVectorB to be initalized first
 	public Matrix getMomentum(int delta){
 		Matrix momTum = new Matrix(parseList.size(), 1);
 		DateTimeFormatter dtf = DateTimeFormat.forPattern("MM/dd/yyyy");
@@ -120,18 +154,18 @@ public class PvmmCurveFitter{
 				}
 			}
 
-			double priceNow = 	closingPriceVector.get(i, 0);
+			double priceNow = 	closingPriceVectorB.get(i, 0);
 			if(flag == 0){//central difference
-				double priceFore = closingPriceVector.get(foreI, 0);
-				double priceBack = closingPriceVector.get(backI, 0);
+				double priceFore = closingPriceVectorB.get(foreI, 0);
+				double priceBack = closingPriceVectorB.get(backI, 0);
 				double centralDiff = (priceFore - priceBack)/(foreDelta + backDelta);
 				momTum.set(i, 0, centralDiff);
 			}else if(flag > 0){//forward difference
-				double priceFore = closingPriceVector.get(foreI, 0);
+				double priceFore = closingPriceVectorB.get(foreI, 0);
 				double forwardDiff = (priceFore - priceNow)/(foreDelta);
 				momTum.set(i, 0, forwardDiff);
 			}else{//backward difference
-				double priceBack = closingPriceVector.get(backI, 0);
+				double priceBack = closingPriceVectorB.get(backI, 0);
 				double backwardDiff = (priceNow - priceBack)/(backDelta);
 				momTum.set(i, 0, backwardDiff);
 			}
@@ -140,6 +174,7 @@ public class PvmmCurveFitter{
 		return momTum;
 	}
 
+	//testing and debugging purposes
 	void sandboxTesting(){
 		//		How to use datetime formatter:
 		DateTimeFormatter dtf = DateTimeFormat.forPattern("MM/dd/yyyy");
